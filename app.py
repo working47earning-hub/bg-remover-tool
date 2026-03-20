0cd3acd0b58e5a0de73cbde42fc98979ef568ab6
working47earning-hub <working47earning@gmail.com>
2026-03-21 00:27:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,4 +30,7 @@ def remove_bg():
     return send_file(byte_io, mimetype='image/png')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    import os
+
+port = int(os.environ.get("PORT", 10000))
+app.run(host="0.0.0.0", port=port)
